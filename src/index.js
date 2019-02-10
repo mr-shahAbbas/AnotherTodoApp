1de1160d139f16b66faa9e447c5727f6824c6bc2
mr.ShahAbbas <abdidevar@gmail.com>
2019-02-10 04:17:24 -0800

    Another

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class Shopping extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1 className="blue"> Todo App</h1>
+            </div>
+        )
+    }
+}
