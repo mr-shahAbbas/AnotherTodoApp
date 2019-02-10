16dc87fe55755acbaabbad68d64599cf4f66d0eb
mr.ShahAbbas <abdidevar@gmail.com>
2019-02-10 04:56:12 -0800

    index has change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-
-class Shopping extends React.Component{
-    render(){
-        return (
-            <div>
-                <h1 className="blue"> Todo App</h1>
-            </div>
-        )
-    }
+class Todo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="blue lighten-3 center-align">Todo App</h1>
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(<Todo />, document.getElementById("root"));
